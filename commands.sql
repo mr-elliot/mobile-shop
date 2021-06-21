91d6de0d4bd4aa7a26b3f43538aaca6e8a27c5c6
mr-elliot <71083233+mr-elliot@users.noreply.github.com>
2021-06-21 15:01:15 +0530

    Add files via upload

---
+++ commands.sql
@@ -0,0 +1,17 @@
+-- give name for table in "table_name"
+
+create table table_name ( Sl_No integer,
+Brand varchar,	Model varchar,	Announced varchar,	Audio_jack varchar,	Battery varchar,
+Bluetooth varchar,	CPU varchar,	Chipset varchar, Colors varchar, Dimensions varchar,Display_type varchar,
+GPU varchar,	Internal_memory varchar,	Loud_speaker varchar,	Memory_card varchar,	Network varchar,	Operating_System varchar,	
+Primary_camera varchar,	RAM varchar,	Radio varchar,	SIM varchar,	Secondary_camera varchar,	Sensors varchar,
+Status varchar,	USB varchar
+);
+
+select * from table_name;
+
+copy mobile_data from '/location/of/file/upload1.csv' delimiter ',' csv header";
+
+select * from table_name;
+
+select * from mobile_data where brand = 'model_name'; -- enter the model name you want information of
